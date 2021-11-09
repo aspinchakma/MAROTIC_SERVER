6460f9a2c9507f8953620a6c15aec388221e3c11
aspinchakma <85893728+aspinchakma@users.noreply.github.com>
2021-11-09 23:55:29 +0600

    basic setup done

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const cors = require("cors");
+const { MongoClient } = require('mongodb');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+require('dotenv').config()
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yu5z2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+
+
+
+
+
+run().catch(console.dir)
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("hello bro, this is basic setup for you .")
+})
+
+
+
+app.listen(port, () => {
+    console.log('I am listening port,', port)
+})
